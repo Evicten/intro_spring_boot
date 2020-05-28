deced1c2815ef2718bca25845c8fea8b76e262e5
Evicten <vicente.c.mendes.pers@tutanota.com>
2020-05-28 13:58:52 +0100

    retornar users com DTO

--- src/main/java/com/sec23/springboot/services/UserService.java
+++ src/main/java/com/sec23/springboot/services/UserService.java
@@ -1,11 +1,13 @@
 package com.sec23.springboot.services;
 
 import com.sec23.springboot.DAO.UserRepository;
+import com.sec23.springboot.domain.DTO.UserDTO;
 import com.sec23.springboot.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
@@ -14,7 +16,7 @@ public class UserService {
     private UserRepository userRepo;
 
 
-    public List<User> findAll(){
-        return userRepo.findAll();
+    public List<UserDTO> findAll(){
+        return userRepo.findAll().stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
     }
 }
